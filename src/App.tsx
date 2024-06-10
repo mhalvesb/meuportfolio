e31e3bde782e18bb1d74130534c6ac346d39bc8e
Matheus Rodrigues <matheus.alvesbr21@gmail.com>
2024-06-10 19:49:39 -0300

    atualização de código do styled.ts

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import {useEffect, useRef} from "react";
 import {motion, useInView, useAnimation} from "framer-motion";
 
 import {Link } from 'react-router-dom';
-import { ActiveLi, ComplementalSection, ContactButton, FirstSection, FirstSectionImg, FiveSection, Footers, FourthSection, FullStack, GrayLi, MessageInput, MyName, NameAndEmailInputs, OtherSkills, Profession, SecondSection, SixLeft, SixRight, SixSection, SkillsDiv, ThirdSection, TwoSkills, TwoSkillsText, WebDev } from './index.styled'
+import { ActiveLi, ComplementalSection, ContactButton, FirstSection, FiveSection, Footers, FourthSection, FullStack, GrayLi, MessageInput, MyName, NameAndEmailInputs, OtherSkills, Profession, SecondSection, SixLeft, SixRight, SixSection, SkillsDiv, ThirdSection, TwoSkills, TwoSkillsText, WebDev } from './index.styled'
 
 
 
