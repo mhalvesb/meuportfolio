bae2de2722105411bb8f407e7dd8475f9b87c83b
Matheus Rodrigues <matheus.alvesbr21@gmail.com>
2024-06-10 09:47:54 -0300

    adicionado projeto

---
+++ src/App.tsx
@@ -0,0 +1,262 @@
+import './App.css'
+
+
+import { BrowserRouter, Link } from 'react-router-dom';
+import { ActiveLi, ComplementalSection, ContactButton, FirstSection, FirstSectionImg, FiveSection, Footers, FourthSection, FullStack, GrayLi, MessageInput, MyName, NameAndEmailInputs, OtherSkills, Profession, SecondSection, SixLeft, SixRight, SixSection, SkillsDiv, ThirdSection, TwoSkills, TwoSkillsText, WebDev } from './index.styled'
+
+function App() {
+
+  return (
+    <>
+      <FirstSection>
+        
+        <Profession>
+          <WebDev>Web Developer</WebDev> <span>/</span> <FullStack>FullStack</FullStack>
+        </Profession>
+        <MyName>Matheus Alves</MyName>
+        <ContactButton>
+          <p>Contact Me</p>
+        </ContactButton>
+        <FirstSectionImg src="./src/assets/images/sec1.png"></FirstSectionImg>
+        
+      </FirstSection>
+
+      <SecondSection>
+        <h3>Responsive Ecommerce Page</h3>
+      <img src="./src/assets/images/LapTopEcommerce.png"/>
+      <span/>
+      </SecondSection>
+      <ThirdSection>
+        <h3>Starbucks Landing Page</h3>
+        <img src="./src/assets/images/mymonitorstarbucks.png"></img>
+        
+      </ThirdSection>
+        
+      <FourthSection>
+          <h3>Responsive for Mobile</h3>
+      </FourthSection>
+
+
+        <ComplementalSection>
+        <h3>More Projects</h3>
+
+<h6>See more</h6>
+
+          <div>
+            <Link to="https://www.starbucks.com">
+              
+              <img src="./src/assets/images/starbucks.png"></img>
+              <p>Starbucks Landing Page</p>
+              <span></span>
+            </Link>
+            <Link to="/">
+            
+              <img src="./src/assets/images/kenosis.png"></img>
+              <p>Civil Construction Page</p>
+              <span></span>
+            </Link>
+            <Link to="/">
+
+              <img src="./src/assets/images/socialMedia1.png"></img>
+              <p>Functional Social Media</p>
+              <span></span>
+            </Link>
+            <Link to="/">
+              <img src="./src/assets/images/img31.png"></img>
+              <p>World-Wide Data</p>
+              <span></span>
+            </Link>
+          </div>
+        </ComplementalSection>
+      <FiveSection>
+        <h3>My Skills</h3>
+
+        <h4>Front-End</h4>
+
+        <SkillsDiv>
+          <div>
+            <p>HTML/CSS</p>
+            <ul>
+              <ActiveLi></ActiveLi>
+              <ActiveLi></ActiveLi>
+              <ActiveLi></ActiveLi>
+              <ActiveLi></ActiveLi>
+              <ActiveLi></ActiveLi>
+              <ActiveLi></ActiveLi>
+              <ActiveLi></ActiveLi>
+              <ActiveLi></ActiveLi>
+              <ActiveLi></ActiveLi>
+              <ActiveLi></ActiveLi>
+            </ul>
+            <span>100%</span>
+          </div>
+
+          <div>
+            <p>JAVASCRIPT</p>
+            <ul>
+              <ActiveLi></ActiveLi>
+              <ActiveLi></ActiveLi>
+              <ActiveLi></ActiveLi>
+              <ActiveLi></ActiveLi>
+              <ActiveLi></ActiveLi>
+              <ActiveLi></ActiveLi>
+              <ActiveLi></ActiveLi>
+              <ActiveLi></ActiveLi>
+              <GrayLi></GrayLi>
+              <GrayLi></GrayLi>
+            </ul>
+            <span>80%</span>
+          </div>
+
+          <div>
+            <p>REACT</p>
+            <ul>
+              <ActiveLi></ActiveLi>
+              <ActiveLi></ActiveLi>
+              <ActiveLi></ActiveLi>
+              <ActiveLi></ActiveLi>
+              <ActiveLi></ActiveLi>
+              <ActiveLi></ActiveLi>
+              <ActiveLi></ActiveLi>
+              <ActiveLi></ActiveLi>
+              <GrayLi></GrayLi>
+              <GrayLi></GrayLi>
+            </ul>
+            <span>80%</span>
+          </div>
+
+          <div>
+            <p>TYPESCRIPT</p>
+            <ul>
+              <ActiveLi></ActiveLi>
+              <ActiveLi></ActiveLi>
+              <ActiveLi></ActiveLi>
+              <ActiveLi></ActiveLi>
+              <ActiveLi></ActiveLi>
+              <GrayLi></GrayLi>
+              <GrayLi></GrayLi>
+              <GrayLi></GrayLi>
+              <GrayLi></GrayLi>
+              <GrayLi></GrayLi>
+            </ul>
+            <span>50%</span>
+          </div>
+
+        </SkillsDiv>
+        
+
+        <TwoSkills>
+          
+          <TwoSkillsText>
+            
+            
+          </TwoSkillsText>
+          
+          <OtherSkills>
+          <h4>Back-End</h4>
+          <div>
+            <p>NODE</p>
+            <ul>
+              <ActiveLi></ActiveLi>
+              <ActiveLi></ActiveLi>
+              <ActiveLi></ActiveLi>
+              <ActiveLi></ActiveLi>
+              <ActiveLi></ActiveLi>
+              <ActiveLi></ActiveLi>
+              <ActiveLi></ActiveLi>
+              <ActiveLi></ActiveLi>
+              <GrayLi></GrayLi>
+              <GrayLi></GrayLi>
+            </ul>
+            <span>80%</span>
+          </div>
+
+          <div>
+            <p>MYSQL</p>
+            <ul>
+              <ActiveLi></ActiveLi>
+              <ActiveLi></ActiveLi>
+              <ActiveLi></ActiveLi>
+              <ActiveLi></ActiveLi>
+              <ActiveLi></ActiveLi>
+              <ActiveLi></ActiveLi>
+              <ActiveLi></ActiveLi>
+              <GrayLi></GrayLi>
+              <GrayLi></GrayLi>
+              <GrayLi></GrayLi>
+            </ul>
+            <span>70%</span>
+          </div>
+        </OtherSkills>
+        <OtherSkills>
+        <h4>Additional</h4>
+          <div>
+            <p>GIT/GITHUB</p>
+            <ul>
+              <ActiveLi></ActiveLi>
+              <ActiveLi></ActiveLi>
+              <ActiveLi></ActiveLi>
+              <ActiveLi></ActiveLi>
+              <ActiveLi></ActiveLi>
+              <ActiveLi></ActiveLi>
+              <ActiveLi></ActiveLi>
+              <ActiveLi></ActiveLi>
+              <ActiveLi></ActiveLi>
+              <ActiveLi></ActiveLi>
+            </ul>
+            <span>100%</span>
+          </div>
+
+          <div>
+            <p>UI/UX</p>
+            <ul>
+              <ActiveLi></ActiveLi>
+              <ActiveLi></ActiveLi>
+              <ActiveLi></ActiveLi>
+              <ActiveLi></ActiveLi>
+              <ActiveLi></ActiveLi>
+              <ActiveLi></ActiveLi>
+              <GrayLi></GrayLi>
+              <GrayLi></GrayLi>
+              <GrayLi></GrayLi>
+              <GrayLi></GrayLi>
+            </ul>
+            <span>60%</span>
+          </div>
+          </OtherSkills>
+        
+
+        </TwoSkills>
+      </FiveSection>
+
+      <SixSection>
+        <SixLeft>
+            <h4> GET IN TOUCH </h4>
+              <div><img src="./src/assets/images/linkedin.png"></img> <p>Matheus Alves</p></div>
+              <div><img src="./src/assets/images/gmail.png"></img> <p>matheus.alvesbr21@gmail.com</p></div>
+              <div><img src="./src/assets/images/github.png"></img> <p>/mhalvesb</p></div>
+              <div><img src="./src/assets/images/instagram.png"></img> <p>matheus.alvesbr21</p></div>
+        </SixLeft>
+
+        <SixRight>
+            <h4>DROP ME A MESSAGE</h4>
+            <NameAndEmailInputs>
+              <input type="text" placeholder="NAME"></input>
+              <input type="text" placeholder="EMAIL"></input>
+            </NameAndEmailInputs>
+            <MessageInput>
+              <textarea placeholder="TYPE MESSAGE"></textarea>
+            </MessageInput>
+            <button>Send</button>
+        </SixRight>
+      </SixSection>
+
+      <Footers>
+        <p>All rights reserved created by <span>Matheus Alves</span> </p>
+        
+      </Footers>
+    </>
+  )
+}
+
+export default App
---
+++ src/index.styled.ts
@@ -0,0 +1,944 @@
+import styled from "styled-components";
+
+
+
+export const FirstSection = styled.section`
+    background-color: #000;
+    height: 100vh;
+    z-index: 1;
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+`
+
+export const FirstSectionImg = styled.img`
+    width: 100%;
+    height: 100vh;
+    z-index: 1;
+    opacity: 1;
+    position: absolute;
+    top: 0;
+    opacity: 0.05;
+    z-index: -1;
+`;
+
+export const WebDev = styled.h3`
+    font-size: 80px;
+    color: #9687F9;
+    margin: 0;
+    padding: 0;
+    
+    @media(max-width: 1024px){
+        font-size: 60px;
+    }
+
+    @media(max-width: 768px){
+        font-size: 30px;
+    }
+    @media (max-width: 425px){
+        font-size: 20px;
+    }
+`;
+
+export const FullStack = styled.h3`
+    font-size: 80px;
+    color: #5CE6BB;
+    margin: 0;
+    padding: 0;
+    @media(max-width: 1024px){
+        font-size: 60px;
+    }
+
+    @media(max-width: 768px){
+        font-size: 30px;
+    }
+    @media (max-width: 425px){
+        font-size: 20px;
+    }
+`;
+
+export const Profession = styled.div`
+    display: flex;
+
+    span{
+        color: #fff;
+        font-size: 80px;
+    }
+
+    @media(max-width: 1024px){
+        span{
+            font-size: 60px;
+        }
+    }
+    @media(max-width: 768px){
+        span{
+            font-size: 30px;
+        }
+        
+    }
+    @media (max-width: 425px){
+        span{
+            font-size: 20px;
+        }
+    }
+`;
+
+
+export const MyName = styled.h1`
+    font-size: 112px;
+    color: #fff;
+    margin: 0;
+    padding: 0;
+
+    @media(max-width: 1024px){
+        font-size: 92px;
+    }
+
+    @media(max-width: 768px){
+        font-size: 60px;
+    }
+    @media(max-width: 425px){
+        font-size: 30px;
+    }
+`;
+
+
+export const ContactButton = styled.button`
+    padding: 15px 50px;
+    background-color: #FDCE2B;
+    border: none;
+    border-radius: 12px;
+    color: #000;
+    font-size: 30px;
+    
+    cursor: pointer;
+
+    p{
+        font-weight: 200;
+        margin: 0;
+        padding: 0;
+    }
+
+    @media (max-width: 1024px){
+        padding: 10px 45px;
+        font-size: 20px;
+    }
+
+    @media (max-width: 768px){
+        padding: 5px 30px;
+        font-size: 15px;
+    }
+`;
+
+export const SecondSection = styled.section`
+    height: 1000px;
+    background: linear-gradient(180deg, #C0C0C0 86%, #000);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    position: relative;
+    padding: 0px 15%;
+
+    h3{
+        z-index: 3;
+        font-size: 70px;
+        margin: 0;
+        padding: 0;
+    }
+    span{
+        width: 800px;
+        height: 800px;
+        z-index: 2;
+        background-color: #fff;
+        filter: blur(80px);
+        display: block;
+        border-radius: 9999px;
+        position: absolute;
+    }
+    img{
+        z-index: 3;
+        position: relative;
+        width: 70%;
+        
+    }
+
+    @media (max-width: 1280px){
+        padding: 0 5%;
+        height: 800px;
+        h3{
+            font-size: 60px;
+        }
+        span{
+        width: 600px;
+        height: 600px;
+        z-index: 2;
+        background-color: #fff;
+        filter: blur(80px);
+        display: block;
+        border-radius: 9999px;
+        position: absolute;
+    }
+
+    @media(max-width: 1024px){
+        height: auto;
+        padding: 100px 5%;
+        h3{
+            font-size: 50px;
+        }
+        
+    }
+
+    @media(max-width: 768px){
+        h3{
+            font-size: 30px;
+        }
+
+        span{
+            width: 200px;
+            height: 200px;
+        }
+    }
+    }
+    @media(max-width: 425px){
+        padding: 50px 5%;
+        h3{
+            font-size: 18px;
+            margin: 10px 0px;
+        }
+    }
+`;
+
+
+export const ThirdSection = styled.section`
+    height: auto;
+    
+    background-color: #000;
+    padding: 100px 15%;
+
+    img{
+        width: 100%;
+    }
+
+    h3{
+        margin: 0px 0px;
+        padding: 40px 0px;
+        color: #fff;
+        font-size: 80px;
+        height: 50px;
+        text-align: center;
+    }
+    @media (max-width: 1280px){
+        padding: 0 5%;
+    }
+
+    @media (max-width: 1024px){
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        padding: 100px 5%;
+        h3{
+            font-size: 55px;
+        }
+        img{
+            width: 80%;
+        }
+    }
+    @media(max-width: 758px){
+        h3{
+            font-size: 35px;
+        }
+    }
+
+    @media(max-width: 425px){
+        padding: 50px 5%;
+        h3{
+            font-size: 20px;
+        }
+    }
+`;
+
+export const ComplementalSection = styled.section`
+    height: 1000px;
+    background: #000;
+    padding: 0px 15%;
+
+    h3{
+        margin: 0px 0px;
+        padding: 40px 0px;
+        color: #fff;
+        font-size: 80px;
+        height: 50px;
+        text-align: center;
+    }
+
+    h6{
+        margin: 0;
+        padding: 0;
+        font-size: 20px;
+        color: #fff;
+        display: block;
+        text-align: right;
+        cursor: pointer;
+    }
+
+    div{
+        margin: auto;
+        display: flex;
+        align-items: center;
+        flex-wrap: wrap;
+        justify-content: space-around;
+    }
+    a{
+        text-decoration: none;
+        margin: 30px 0px;
+        position: relative;
+        z-index: 2;
+    }
+    p{
+        color: #fff;
+        text-decoration: none;
+        text-align: center;
+        font-size: 20px;
+        padding: 0;
+        margin: 0px;
+        margin-bottom: 0px;
+        position: relative;
+        z-index: 2;
+    }
+
+
+    img{
+        margin: 0px 0px;
+        position: relative;
+        z-index: 2;
+    }
+
+    span{
+        width: 50%;
+        aspect-ratio: 1/1;
+        background-color: #fff;
+        position: absolute;
+        top: 0;
+        transform: translate(50%, -5%);
+        z-index: 0;
+        border-radius: 9999px;
+        opacity: 0.2;
+        filter: blur(80px);
+    } 
+
+    @media(max-width: 1500px){
+        img{
+            width: 400px;
+    }
+
+    @media(max-width: 1280px){
+        padding: 0 5%;
+        img{
+            width: 420px;
+    }
+    }
+
+    @media(max-width: 1024px){
+        display: flex;
+        flex-direction: column;
+        align-items: stretch;
+        justify-content: center;
+        img{
+            width: 300px;
+        }
+        h3{
+            font-size: 70px;
+        }
+        div{
+            margin: 0;
+        }
+    }
+    }
+
+    @media (max-width: 768px){
+        padding: 100px 5%;
+        div{
+            flex-direction: column;
+    }
+    h3{
+            font-size: 50px;
+        }
+    }
+
+    @media(max-width: 425px){
+        height: auto;
+        justify-content: center;
+        padding: 25px 5%;
+        h3{
+            font-size: 30px;
+        }
+        img{
+            width: 150px;
+            display: block;
+            margin: auto;
+        }
+        h6{
+            font-size: 12px;
+        }
+        p{
+            font-size: 14px;
+            margin: 10px 0px;
+        }
+    }
+`;
+
+
+export const FourthSection = styled.section`
+    height: 1200px;
+    background-color: #C0C0C0;
+    background: url("./src/assets/images/app.jpg");
+    background-position: center;
+    background-size: cover;
+    background-repeat: no-repeat;
+
+    h3{
+        margin: 0;
+        padding: 0;
+        font-size: 100px;
+        text-align: center;
+        color: #fff;
+    }
+
+    @media(max-width: 1280px){
+        h3{
+            font-size: 80px;
+        }
+    }
+
+    @media(max-width: 1024px){
+        h3{
+            font-size: 70px;
+        }
+    }
+    @media(max-width: 768px){
+        height: 800px;
+        h3{
+            padding-top: 50px;
+            font-size: 42px;
+        }
+    }
+
+    @media(max-width: 425px){
+        height: 650px;
+        h3{
+            font-size: 27px;
+        }
+    }
+`;
+
+
+export const FiveSection = styled.section`
+    height: auto;
+    padding: 0 15%;
+    background: url("./src/assets/images/coding.png");
+    background-color: #000;
+    background-position: center;
+    background-size: cover;
+    background-repeat: no-repeat;
+
+    h3{
+        margin: 0;
+        padding: 0;
+        font-size: 100px;
+        text-align: center;
+        color: #fff;
+    }
+
+    h4{
+        margin: 0;
+        padding: 0;
+        font-size: 80px;
+        text-align: center;
+        color: #fff;
+        height: 120px;
+    }
+    @media (max-width: 1280px){
+        padding: 0 5%;
+    }
+    @media (max-width: 1024px){
+        h3{
+            font-size: 80px;
+        }
+
+        h4{
+            font-size: 60px;
+        }
+    }
+
+    @media (max-width: 768px){
+        height: auto;
+        h4{
+            font-size: 40px;
+        }
+    }
+    @media(max-width: 425px){
+        h3{
+            font-size: 40px;
+        }
+        h4{
+            font-size: 25px;
+            height: auto;
+        }
+    }
+`;
+
+export const SkillsDiv = styled.div`
+    display: flex;
+    flex-direction: row;
+    width: 100%;
+    align-items: center;
+    justify-content: space-between;
+    flex-wrap: wrap;
+    p{
+        color: #5CE6BB;
+        text-align: center;
+        font-size: 40px;
+        margin: 0;
+        padding: 0;
+        height: 70px;
+    }
+    div{
+        width: 250px;
+        height: 250px;
+        border: 1px solid #fff;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+    }
+
+    h4{
+        display: block;
+        width: 100%;
+    }
+
+    ul, li{
+        margin: 0;
+        padding: 0;
+    }
+    li{
+        width: 6px;
+        height: 40px;
+    }
+    ul{
+        list-style-type: none;
+        display: flex;
+        gap: 11px;
+        alig-items: center;
+        justify-content: center;
+    }
+
+    
+
+    span{
+        display: block;
+        color: #fff;
+        text-align: end;
+        font-size: 15px;
+        margin-right: 44px;
+        align-self: flex-end;
+        color: #fff;
+    }
+
+    @media(max-width: 1500px){
+        div{
+        width: 200px;
+        height: 200px;
+        }
+    }
+
+    @media(max-width: 1024px){
+        div{
+            width: 150px;
+            height: 150px;
+        }
+        ul{
+            gap: 9px;
+        }
+        li{
+            width: 5px;
+            height: 35px;
+        }
+        p{
+            font-size: 26px;
+            height: auto;
+        }
+        span{
+            margin-right: 8px;
+        }
+    }
+
+    @media(max-width: 768px){
+        flex-wrap: wrap;
+        align-items: center;
+        justify-content: center;
+        
+        div{
+            width: 150px;
+            height: 150px;
+            margin: 20px 10px;
+        }
+        ul{
+            gap: 9px;
+        }
+        li{
+            width: 4px;
+            height: 35px;
+        }
+        p{
+            font-size: 25px;
+        }
+        span{
+            font-size: 15px;
+            margin-right: 18px;
+        }
+        
+    }
+    @media(max-width: 425px){
+        p{
+            font-size: 20px;
+        }
+        li{
+            width: 3px;
+            height: 30px;
+        }
+        span{
+            font-size: 12px;
+        }
+    }
+`;
+
+export const OtherSkills = styled.div`
+        display: flex;
+    flex-direction: row;
+    width: 100%;
+    align-items: center;
+    justify-content: center;
+    gap: 20px;
+    flex-wrap: wrap;
+    p{
+        color: #5CE6BB;
+        text-align: center;
+        font-size: 40px;
+        margin: 0;
+        padding: 0;
+        height: 70px;
+    }
+    div{
+        width: 250px;
+        height: 250px;
+        border: 1px solid #fff;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+    }
+
+    h4{
+        display: block;
+        width: 100%;
+    }
+
+    ul, li{
+        margin: 0;
+        padding: 0;
+    }
+    li{
+        width: 6px;
+        height: 40px;
+    }
+    ul{
+        list-style-type: none;
+        display: flex;
+        gap: 11px;
+        alig-items: center;
+        justify-content: center;
+    }
+
+    
+
+    span{
+        display: block;
+        color: #fff;
+        text-align: end;
+        font-size: 15px;
+        margin-right: 44px;
+        align-self: flex-end;
+        color: #fff;
+    }
+
+    @media(max-width: 1500px){
+        div{
+            width: 200px;
+        height: 200px;
+        }
+    }
+
+    @media(max-width: 1024px){
+        div{
+            width: 150px;
+            height: 150px;
+        }
+        ul{
+            gap: 9px;
+        }
+        li{
+            width: 5px;
+            height: 35px;
+        }
+        p{
+            font-size: 26px;
+            height: auto;
+        }
+        span{
+            margin-right: 8px;
+        }
+    }
+
+    @media(max-width: 768px){
+        flex-wrap: wrap;
+        align-items: center;
+        justify-content: center;
+        
+        div{
+            width: 150px;
+            height: 150px;
+            margin: 20px 10px;
+        }
+        ul{
+            gap: 9px;
+        }
+        li{
+            width: 4px;
+            height: 35px;
+        }
+        p{
+            font-size: 25px;
+        }
+        span{
+            font-size: 15px;
+            margin-right: 18px;
+        }
+        
+    }
+`;
+
+
+export const ActiveLi = styled.li`
+        
+        background-color: #5CE6BB;
+`;
+
+export const GrayLi = styled.li`
+        background-color: #fff;
+`;
+
+
+export const TwoSkills = styled.div`
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    margin-top: 50px;
+    `;
+
+
+export const TwoSkillsText = styled.div`
+    display: flex;
+    width: 100%;
+    justify-content: space-around;
+`;
+
+
+export const SixSection = styled.section`
+    height: auto;
+    display: flex;
+    justify-content: space-between;
+    background-color: #000;
+    color: #fff;
+    padding: 100px 15%;
+
+    @media(max-width: 1280px){
+        padding: 100px 5%;   
+    }
+    @media(max-width: 768px){
+        padding: 20px 5%;
+        flex-direction: column;
+    }
+
+`;
+
+export const SixLeft = styled.div`
+    display: flex;
+        flex-direction: column;
+        flex: 1;
+    h4{
+        font-size: 45px;
+        margin:0;
+        padding: 0;
+    }
+
+    div{
+        display: flex;
+        align-items: center;
+        cursor: pointer;
+        margin: 10px 0px;
+        img{
+            width: 64px;
+            height: 64px;
+        }
+
+        p{
+        font-size: 20px;
+        margin-left: 10px;
+    }
+    }
+
+    @media(max-width: 1024px){
+        h4{
+            font-size: 35px;
+        }
+        div{
+
+        img{
+            width: 48px;
+            height: 48px;
+        }
+
+        p{
+            font-size: 15px;
+        }
+        }
+    }
+
+    @media (max-width: 768px){
+        margin: 50px 0px;
+        h4{
+            font-size: 25px;
+        }
+    }
+    
+`;
+
+export const SixRight = styled.div`
+    display: flex;
+    flex-direction: column;
+    flex: 1;
+    h4{
+        margin: 0;
+        padding:0; 
+        font-size: 45px;
+    }
+
+    button{
+        height: 50px;
+        width: 200px;
+        border-radius: 10px;
+        font-weight: 100;
+        background-color: #FDCE2B;
+        border: none;
+        cursor: pointer;
+    }
+
+    @media(max-width: 1024px){
+        h4{
+            font-size: 35px;
+        }  
+
+        button{
+            height: 40px;
+            width: 180px;
+        }
+    }
+    @media (max-width: 768px){
+        h4{
+            font-size: 25px;
+        }
+    }
+    
+    
+
+    
+`;
+
+export const NameAndEmailInputs = styled.div`
+    display: flex;
+    justify-content: space-between;
+
+    input{
+        width: 45%;
+        background: transparent;
+        border: none;
+        border-bottom: 1px solid #fff;
+        outline: none;
+        font-size: 20px;
+        font-weight: 100;
+        line-height: 50px;
+        color: #fff;
+
+        &::placeholder{
+        color: #fff;
+    }
+    }
+    margin-bottom: 100px;
+
+    @media(max-width: 1024px){
+        input{
+            line-height: 40px;
+            font-size: 15px;
+        }
+    }
+
+`;
+
+
+export const MessageInput = styled.div`
+
+
+    width: 100%;
+    textarea{
+        width: 100%;
+        background: transparent;
+        border: none;
+        border-bottom: 1px solid #fff;
+        line-height: 60px;
+        resize: none;
+        outline: none;
+        font-size: 20px;
+        color: #fff;
+        font-weight: 100;
+
+        &::placeholder{
+        color: #fff;
+    }
+    }
+    @media(max-width: 1024px){
+        textarea{
+            font-size: 15px;
+            line-height: 40px;
+    }
+    }
+`;
+
+export const Footers = styled.footer`
+    background-color: #111;
+    text-align: center;
+    
+    p, span{
+        margin: 0;
+        padding: 0;
+        display: inline-block;
+        color: #fff;
+    }
+
+    span{
+        color: #fff;
+    }
+`;
